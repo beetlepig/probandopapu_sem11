3bec4236d37b4c793f578b458e1f7a46a075050b
1107513806 <1107513806@SC309C20.academico.icesi.edu.co>
2016-04-05 09:46:08 -0500

    modificacion dos papu

--- GitInicial_Vallejo/src/Logica.java
+++ GitInicial_Vallejo/src/Logica.java
@@ -1,4 +1,8 @@
 
 public class Logica {
 
+	
+	public Logica(){
+	
+	}
 }
--- GitInicial_Vallejo/src/MainApp.java
+++ GitInicial_Vallejo/src/MainApp.java
@@ -4,4 +4,8 @@ public class MainApp extends PApplet {
 public void setup(){
 	size(200,200);
 }
+
+public void draw(){
+	background(255,0,0);
+}
 }
